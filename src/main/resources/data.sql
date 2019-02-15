39c06afd412f05f772cbae6a9861f8aceee026fa
tahir.appiskey <tahir.saleem@appiskey.com>
2019-02-15 14:34:52 +0500

    update data seeker of permission class

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,30 @@
-
 --
 -- Dumping data for table `permissaion`
 --
 
-INSERT INTO `role` (`id`, `name`) VALUES
-(unhex(replace('a21f6182-0d26-11e9-ab14-d663bd870001','-','User')), 'USER'),
-(unhex(replace('a21f642a-0d26-11e9-ab14-d663bd870002','-','')), 'ADMIN'),
-(unhex(replace('a21f657e-0d26-11e9-ab14-d663bd870003','-','')), 'MANAGER'),
-(unhex(replace('a21f68da-0d26-11e9-ab14-d663bd870004','-','')), 'SUPPERVISOR'),
-(unhex(replace('a21f6a7e-0d26-11e9-ab14-d663bd870005','-','')), 'CLIENT'),
-(unhex(replace('a21f6bb4-0d26-11e9-ab14-d663bd870006','-','')), 'WORKER'),
-(unhex(replace('a21f6f38-0d26-11e9-ab14-d663bd870007','-','')), 'ANDROID'),
-(unhex(replace('a21f708c-0d26-11e9-ab14-d663bd870008','-','')), 'IMAWEB'),
-(unhex(replace('25c1eac8-0d27-11e9-ab14-d663bd870009','-','')), 'IMADMIN'),
-(unhex(replace('25c1f036-0d27-11e9-ab14-d663bd870010','-','')), 'IOS');
-
+INSERT INTO `permission` (`id`, `parent_id`, `name`, `description`) VALUES
+-- ======================================== User Module ========================================
+-- --------------------------------------- Child Permission -----------------------------------
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', '')), '', 'USER', 'USER MODULE'),
+-- --------------------------------------- Child Permission -----------------------------------
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000101', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'CREATE', 'USER MODULE CREATE'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000102', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'UPDATE', 'USER MODULE UPDATE'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000103', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'DELETE', 'USER MODULE DELETE'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000104', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'LIST', 'USER MODULE LIST'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000105', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'LOGIN', 'USER MODULE LOGIN'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000106', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'REGISTER', 'USER MODULE REGISTER'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000107', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'RESET_PASSWORD', 'USER MODULE RESET_PASSWORD'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000108', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'RESET_PASSWORD', 'USER MODULE RESET_PASSWORD'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000109', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'UPDATE_STATUS', 'USER MODULE UPDATE_STATUS'),
+  (unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000110', '-', '')), unhex(replace('a21f6182-0d26-11e9-ab14-d663bd000100', '-', ''))
+    , 'UNLOCK_SCREEN', 'USER MODULE UNLOCK_SCREEN');
+-- ======================================== User Module ========================================
